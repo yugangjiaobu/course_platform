b76fd137efbe534a7086ea8814a4a0a29a520264
10000xiaoyu1 <22301026@bjtu.edu.cn>
2024-10-11 14:56:54 +0800

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/util/DatabaseUtil.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/smart";
+    private static final String USER = "root";
+    private static final String PASSWORD = "M13yS19Q17L12_";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
