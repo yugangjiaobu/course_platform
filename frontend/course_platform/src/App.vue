88fede4c539718f7e1a6b5da2e4a7f0c252c0232
llll129898213 <12425562+llll129898213@user.noreply.gitee.com>
2024-10-15 18:15:33 +0800

    build vue project

---
+++ frontend/course_platform/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import LoginPage from './components/LoginPage.vue'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+	#app{
+		position: fixed;
+	}
+</style>
