f60d3aa790d697a22b14e426a6c2d9206aca6d0c
liuu75001 <150922210+liuu75001@users.noreply.github.com>
2024-10-23 20:07:21 +0800

    Add files via upload

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.backend.service;
 
+import com.example.backend.dto.UserInfoDTO;
 import com.example.backend.entity.User;
 
 public interface UserService {
     User login(String userID, String password);
+    UserInfoDTO getUserInfo(String userId) throws Exception;
+
 }
