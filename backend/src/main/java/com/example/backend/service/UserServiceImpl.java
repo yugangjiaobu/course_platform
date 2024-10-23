1b79ca1ccd1e631f38bafcee3764241507d83264
liuu75001 <150922210+liuu75001@users.noreply.github.com>
2024-10-23 20:15:14 +0800

    Add files via upload

--- backend/src/main/java/com/example/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 import com.example.backend.dao.UserDAO;
+import com.example.backend.dto.UserInfoDTO;
 import com.example.backend.entity.User;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,6 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDAO userDAO;
-
     @Override
     public User login(String username, String password) {
         User user = userDAO.findByUsername(username);
@@ -21,5 +21,17 @@ public class UserServiceImpl implements UserService {
             return user; // 确保user包含角色信息
         }
         return null;
+    }
+    @Override
+    public UserInfoDTO getUserInfo(String userId) throws Exception {
+        User user = userDAO.getUserById(userId);
+        if (user == null) {
+            throw new Exception("用户不存在");
+        }
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setUsername(user.getUserId());
+        userInfoDTO.setEmail(user.getEmail());
+        userInfoDTO.setPhone(user.getPhone());
+        return userInfoDTO;
     }
 }
