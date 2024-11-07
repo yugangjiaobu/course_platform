0d83b2fe629e140fd2fc0cb20c1a22ac78e35563
yugangjiaobu <160090250+yugangjiaobu@users.noreply.github.com>
2024-11-08 01:09:29 +0800

    Update UserService.java

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,10 +1,8 @@
 package com.example.backend.service;
 
 import com.example.backend.dto.UserInfoDTO;
-import com.example.backend.entity.User;
 
 public interface UserService {
-    User login(String userID, String password);
     UserInfoDTO getUserInfo(String userId) throws Exception;
-
+    boolean updateUserInfo(String userId, UserInfoDTO userInfo) throws Exception;
 }
