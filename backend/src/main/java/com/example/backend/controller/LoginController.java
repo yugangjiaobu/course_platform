1416354a4ab5acafa52e92081cc0a8a9e1b0dcb2
yugangjiaobu <160090250+yugangjiaobu@users.noreply.github.com>
2024-11-08 01:10:50 +0800

    Update LoginController.java

--- backend/src/main/java/com/example/backend/controller/LoginController.java
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.LoginDTO;
 import com.example.backend.entity.User;
-import com.example.backend.service.UserService;
+import com.example.backend.service.LoginService;
 import com.example.backend.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +15,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8080") // 替换为你的前端地址
+@CrossOrigin(origins = "http://localhost:8082") // 替换为你的前端地址
 public class LoginController {
     @Autowired
-    private UserService userService;
+    private LoginService loginService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO) {
@@ -27,9 +27,9 @@ public class LoginController {
             String userID = loginDTO.getUserID();
             String password = loginDTO.getPassword();
 
-            User user = userService.login(userID, password);
+            User user = loginService.login(userID, password);
             if (user != null) {
-                String token = JWTUtil.generateToken(user.getName(), user.getRole()); // 传入角色
+                String token = JWTUtil.generateToken(user.getUserId(), user.getRole()); // 传入角色
                 return ResponseEntity.ok().body(token);
             } else {
                 return ResponseEntity.badRequest().body("登录失败：用户名或密码错误");
