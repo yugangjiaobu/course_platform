e32c0f0d78f7c2eef9e55d4e4f965f5055a456b2
yugangjiaobu <160090250+yugangjiaobu@users.noreply.github.com>
2024-11-12 20:46:26 +0800

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/dto/NotificationDTO.java
@@ -0,0 +1,33 @@
+package com.example.backend.dto;
+
+import java.sql.Timestamp;
+
+public class NotificationDTO {
+    private String content;
+    private Timestamp sentAt;
+    private String senderName; // 老师名字
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(Timestamp sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+}
