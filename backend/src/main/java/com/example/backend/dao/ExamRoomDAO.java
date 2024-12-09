f8072b92a09e7e2e18367cfa9182ef297e91ac4c
yugangjiaobu <160090250+yugangjiaobu@users.noreply.github.com>
2024-12-09 11:25:11 +0800

    Update ExamRoomDAO.java

--- backend/src/main/java/com/example/backend/dao/ExamRoomDAO.java
+++ backend/src/main/java/com/example/backend/dao/ExamRoomDAO.java
@@ -2,12 +2,11 @@ package com.example.backend.dao;
 
 import com.example.backend.entity.ExamRoom;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ExamRoomDAO {
-    void addExamRoom(ExamRoom examRoom) throws Exception;
-    ExamRoom getExamRoomById(int roomId) throws Exception;
-    List<ExamRoom> getAllExamRooms() throws Exception;
-    void updateExamRoom(ExamRoom examRoom) throws Exception;
-    void deleteExamRoom(int roomId) throws Exception;
+    void save(ExamRoom examRoom) throws SQLException;
+    ExamRoom getById(String roomId) throws SQLException;
+    ExamRoom getByExamId(String examId) throws SQLException;
 }
